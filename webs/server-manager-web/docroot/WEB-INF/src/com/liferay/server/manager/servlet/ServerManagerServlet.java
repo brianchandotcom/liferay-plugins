814fb40c3ae5ef74b1a0ecd1903a39337fc379a4
Jonathan Potter <jonathan.potter@liferay.com>
2011-06-29 10:54:02 -0700

    More improvements

--- webs/server-manager-web/docroot/WEB-INF/src/com/liferay/server/manager/servlet/ServerManagerServlet.java
+++ webs/server-manager-web/docroot/WEB-INF/src/com/liferay/server/manager/servlet/ServerManagerServlet.java
@@ -3,35 +3,41 @@ package com.liferay.server.manager.servlet;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.ServerDetector;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import javax.management.MBeanServer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.Manager;
-import org.apache.catalina.core.StandardHost;
-import org.apache.catalina.startup.HostConfig;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 
 public class ServerManagerServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response)
+	protected void service(
+		HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 
 		PrintWriter out = response.getWriter();
@@ -46,72 +52,235 @@ public class ServerManagerServlet extends HttpServlet {
 
 		response.setContentType("application/json");
 
-		if (request.getMethod().equalsIgnoreCase("get")) {
-			handleGet(request, response, pathPieces);
-		} else if (request.getMethod().equalsIgnoreCase("post")) {
-			handlePost(request, response, pathPieces);
+		try {
+			if (request.getMethod().equalsIgnoreCase("get")) {
+				handleGet(request, response, pathPieces);
+			}
+			else if (request.getMethod().equalsIgnoreCase("post")) {
+				handlePost(request, response, pathPieces);
+			}
+			else if (request.getMethod().equalsIgnoreCase("put")) {
+				handlePut(request, response, pathPieces);
+			}
+			else if (request.getMethod().equalsIgnoreCase("delete")) {
+				handleDelete(request, response, pathPieces);
+			}
+		} catch (Exception e) {
+			_log.error(e);
 		}
 
 		out.flush();
 		out.close();
 	}
 
-	protected void handleGet (HttpServletRequest request, HttpServletResponse response, String[] path) throws IOException {
+	protected void handleGet(
+		HttpServletRequest request, HttpServletResponse response, String[] path)
+		throws IOException {
+
 		if (path.length == 0) {
 			return;
 		}
 
 		if (path[0].equals("is-alive")) {
-			// Test JMX
-//			ArrayList<?> list = MBeanServerFactory.findMBeanServer(null);
-//			MBeanServer mbeanServer = (MBeanServer) list.get(0);
-//			System.out.println("MBeanServer: " + mbeanServer);
-//			String domain = mbeanServer.getDefaultDomain();
-//			System.out.println("Default domain: " + domain);
-//			Set<?> namesSet = mbeanServer.queryNames(null, null);
-//			Object[] objectNames = namesSet.toArray();
-//			System.out.println("mbean object names:");
-//
-//			for (int x = 0; x < objectNames.length; x++) {
-//				System.out.println(objectNames[x]);
-//			}
-
-
-
 			isAliveHandler(request, response);
+		} else if (path[0].equals("log")) {
+			logHandler(request, response);
+		} else if (path[0].equals("debug-port")) {
+			debugPortHandler(request, response);
 		}
 	}
 
-
-	/**
-	 * Invoke the check method on the deployer.
-	 */
-	protected void check(String name, MBeanServer mBeanServer)
+	protected void handlePost(
+		HttpServletRequest request, HttpServletResponse response, String[] path)
 		throws Exception {
 
-		String[] params = {
-			name
-		};
+		if (path.length == 0) {
+			return;
+		}
 
-		String[] signature = {
-			"java.lang.String"
-		};
-
-//		mBeanServer.invoke(oname, "check", params, signature);
+		if (path[0].equals("deploy")) {
+			deployHandler(request, response, path);
+		}
 	}
 
-	/**
-	 * Upload the WAR file included in this request, and store it at the
-	 * specified file location.
-	 *
-	 * @param writer    Writer to render to
-	 * @param request   The servlet request we are processing
-	 * @param war       The file into which we should store the uploaded WAR
-	 * @param smClient  The StringManager used to construct i18n messages based
-	 *                  on the Locale of the client
-	 *
-	 * @exception IOException if an I/O error occurs during processing
-	 */
+	protected void handlePut (HttpServletRequest request, HttpServletResponse response, String[] path) throws Exception {
+		if (path.length == 0) {
+			return;
+		}
+	}
+
+	protected void handleDelete (HttpServletRequest request, HttpServletResponse response, String[] path) throws Exception {
+		if (path.length == 0) {
+			return;
+		}
+
+		if (path[0].equals("undeploy")) {
+			undeployHandler(request, response, path);
+		}
+	}
+
+	protected void isAliveHandler(
+		HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		response.getWriter().print("1");
+	}
+
+	protected void deployHandler(
+			HttpServletRequest request, HttpServletResponse response, String[] path)
+		throws Exception {
+
+//		if (path.length < 2) {
+//			return;
+//		}
+//
+//		String contextName = path[1];
+
+		List<FileItem> fileItems = getFileItems(request);
+
+		for (FileItem fileItem : fileItems) {
+			InputStream is = fileItem.getInputStream();
+
+			String deployDir = PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_DEPLOY_DIR);
+
+			if (deployDir == null) {
+				return;
+			}
+
+			File target =
+				new File(deployDir + "/" + fileItem.getName());
+
+			uploadWar(is, target);
+
+			response.getWriter().print("deployed " + fileItem.getName());
+
+			break;
+		}
+
+	}
+
+	protected void undeployHandler(
+		HttpServletRequest request, HttpServletResponse response, String[] path) {
+
+		if (path.length < 2) {
+			return;
+		}
+
+		String context = path[1];
+		String webappsDirectory = getWebappsDirectory();
+
+		File directory = new File(webappsDirectory + "/" + context);
+
+		if (directory.exists()) {
+			boolean success = FileUtils.deleteQuietly(directory);
+			if (!success) {
+				_log.info("Could not remove " + context);
+			}
+		}
+	}
+
+	protected void logHandler (HttpServletRequest request, HttpServletResponse response) throws IOException {
+//		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        Date date = new Date();
+//
+//        String dateString = dateFormat.format(date);
+//
+//		File log = new File(System.getProperty("catalina.base") + "/logs/catalina." + dateString + ".log");
+		File log = new File(System.getProperty("catalina.base") + "/logs/catalina.out");
+
+		if (!log.exists()) {
+			return;
+		}
+
+		BufferedReader reader = new BufferedReader(new FileReader(log));
+
+		while (true) {
+			String line = reader.readLine();
+
+			if (line == null) {
+				break;
+			}
+
+			response.getWriter().write(line + "\n");
+		}
+	}
+
+	protected void debugPortHandler (HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String debugPort = "";
+
+		String catalinaOptionString = System.getProperty("env.CATALINA_OPTS");
+		String[] options = catalinaOptionString.split("\\s*-D?");
+
+		for (String option : options) {
+			if (option.contains("agentlib:jdwp")) {
+				Pattern pattern = Pattern.compile("address=(\\d+)");
+				Matcher matcher = pattern.matcher(option);
+
+				if (matcher.find()) {
+					debugPort = matcher.group(1);
+				}
+			}
+		}
+
+		response.getWriter().write(debugPort);
+	}
+
+	protected String getWebappsDirectory() {
+		try {
+			if (ServerDetector.isGeronimo()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_GERONIMO_DEST_DIR);
+			}
+			else if (ServerDetector.isGlassfish()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_GLASSFISH_DEST_DIR);
+			}
+			else if (ServerDetector.isJBoss()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_JBOSS_DEST_DIR);
+			}
+			else if (ServerDetector.isJetty()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_JETTY_DEST_DIR);
+			}
+			else if (ServerDetector.isJOnAS()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_JONAS_DEST_DIR);
+			}
+			else if (ServerDetector.isResin()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_RESIN_DEST_DIR);
+			}
+			else if (ServerDetector.isTomcat()) {
+				return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_TOMCAT_DEST_DIR);
+			}
+			else if (ServerDetector.isWebLogic()) {
+				// return PrefsPropsUtil.getString(PropsKeys.AUTO_DEPLOY_WEBLOGIC_DEST_DIR);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+
+		return null;
+	}
+
+	protected List<FileItem> getFileItems (HttpServletRequest request) {
+		List<FileItem> results = new ArrayList<FileItem>();
+
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload uploader = new ServletFileUpload(factory);
+
+		try {
+			@SuppressWarnings("unchecked")
+			List<FileItem> fileItems = uploader.parseRequest(request);
+
+			for (FileItem fileItem : fileItems) {
+				if (!fileItem.isFormField()) {
+					results.add(fileItem);
+				}
+			}
+		} catch (FileUploadException e) {
+			_log.error(e);
+		}
+
+		return results;
+	}
+
 	protected void uploadWar(InputStream istream, File war)
 		throws IOException {
 
@@ -120,8 +289,7 @@ public class ServerManagerServlet extends HttpServlet {
 		}
 		BufferedOutputStream ostream = null;
 		try {
-			ostream =
-				new BufferedOutputStream(new FileOutputStream(war), 1024);
+			ostream = new BufferedOutputStream(new FileOutputStream(war), 1024);
 			byte buffer[] = new byte[1024];
 			while (true) {
 				int n = istream.read(buffer);
@@ -136,16 +304,19 @@ public class ServerManagerServlet extends HttpServlet {
 			ostream = null;
 			istream.close();
 			istream = null;
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			if (war.exists() && !war.delete()) {
 				throw new IOException("Failed to delete file: " + war);
 			}
 			throw e;
-		} finally {
+		}
+		finally {
 			if (ostream != null) {
 				try {
 					ostream.close();
-				} catch (Throwable t) {
+				}
+				catch (Throwable t) {
 					_log.error(t);
 				}
 				ostream = null;
@@ -153,7 +324,8 @@ public class ServerManagerServlet extends HttpServlet {
 			if (istream != null) {
 				try {
 					istream.close();
-				} catch (Throwable t) {
+				}
+				catch (Throwable t) {
 					_log.error(t);
 				}
 				istream = null;
@@ -161,122 +333,7 @@ public class ServerManagerServlet extends HttpServlet {
 		}
 	}
 
-
-
-	protected void handlePost (HttpServletRequest request, HttpServletResponse response, String[] path) throws IOException {
-		if (path.length == 0) {
-			return;
-		}
-
-		if (path[0].equals("deploy")) {
-			if (ServletFileUpload.isMultipartContent(request)) {
-				DiskFileItemFactory factory = new DiskFileItemFactory();
-				ServletFileUpload uploader = new ServletFileUpload(factory);
-
-				try {
-					@SuppressWarnings("unchecked")
-					List<FileItem> fileItems = uploader.parseRequest(request);
-
-					for (FileItem fileItem : fileItems) {
-						if (fileItem.isFormField()) {
-						} else {
-							// File
-							InputStream is = fileItem.getInputStream();
-
-							StandardHost host = new StandardHost();
-							Manager manager = host.getManager();
-							HostConfig hc = new HostConfig();
-
-							System.out.println("DEBUG: appbase: " + host.getAppBase());
-							System.out.println("DEBUG: working dir: " + System.getProperty("user.dir"));
-							System.out.println("DEBUG: context path: " + request.getContextPath());
-
-							// Strip off the current context
-							String webappsDir = this.getServletContext().getRealPath("").replaceFirst("[^/]+/?$", "");
-
-
-//							MBeanServer mBeanServer = Registry.getRegistry(null, null).getMBeanServer();
-
-							File file = new File(webappsDir + "/test.war");
-
-							System.out.println("DEBUG: war file location: " + file.getAbsolutePath());
-
-							uploadWar(is, file);
-
-//							List<String> wars = new ArrayList<String>();
-//							wars.add(file.getAbsolutePath());
-//							BaseDeployer bd = new BaseDeployer(wars, null);
-//							bd.deploy();
-						}
-					}
-				}
-				catch (FileUploadException e) {
-					_log.error(e);
-					return;
-				}
-
-				deployHandler(request, response, path);
-			}
-		}
-	}
-
-	protected void deployHandler (HttpServletRequest request, HttpServletResponse response, String[] path) throws IOException {
-		if (path.length < 2) {
-			return;
-		}
-
-		String contextName = path[1];
-
-		Object war = request.getParameter("deploy-war");
-
-		response.getWriter().print("deploying " + contextName);
-
-	}
-
-	protected void isAliveHandler (HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().print("1");
-	}
-
-//	@Override
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//		throws ServletException, IOException {
-//
-//		PrintWriter out = response.getWriter();
-//		String path = request.getServletPath().toLowerCase();
-//		String[] pathPieces = path.split("/");
-//
-//		response.setContentType("application/json");
-//
-//		if (pathPieces.length == 0) {
-//			return;
-//		}
-//
-//		if (path.startsWith("/is-alive")) {
-//			isAliveHandler(request, response);
-//		}
-//
-//		out.flush();
-//		out.close();
-//	}
-//
-//	@Override
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//		throws ServletException, IOException {
-//
-//		PrintWriter out = response.getWriter();
-//		String path = request.getServletPath().toLowerCase();
-//
-//		response.setContentType("application/json");
-//
-//		if (path.startsWith("/deploy")) {
-//			isAliveHandler(request, response);
-//		}
-//
-//		out.flush();
-//		out.close();
-//	}
-
-
-	protected static Log _log = LogFactoryUtil.getLog(ServerManagerServlet.class);
+	protected static Log _log =
+		LogFactoryUtil.getLog(ServerManagerServlet.class);
 
 }
