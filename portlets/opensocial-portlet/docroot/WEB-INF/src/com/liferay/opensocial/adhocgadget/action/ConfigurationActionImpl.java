c52ad0504732cc59d4b4a9031c32e96c4bcd6580
Hugo Huijser <hugo.huijser@liferay.com>
2013-06-28 13:59:03 -0700

    LPS-32066 Fix compile error

--- portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/adhocgadget/action/ConfigurationActionImpl.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/adhocgadget/action/ConfigurationActionImpl.java
@@ -122,7 +122,7 @@ public class ConfigurationActionImpl extends BaseConfigurationAction {
 
 		PortletPreferences preferences = portletRequest.getPreferences();
 
-		return ShindigUtil.getGadget(portletPreferences);
+		return ShindigUtil.getGadget(preferences);
 	}
 
 }
