793a6309d4c5811cd541e9cd58be15446ac6e27c
Philippe Proulx <philippe.proulx@savoirfairelinux.com>
2013-10-09 16:41:46 -0400

    chat-video-portlet: add file headers

--- portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/WebRtcClient.java
+++ portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/WebRtcClient.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2013 Savoir-faire Linux
+ *     Philippe Proulx <philippe.proulx@savoirfairelinux.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by Savoir-faire Linux.  The name of Savoir-faire Linux
+ * may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 package com.liferay.chat.video;
 
 import java.util.ArrayList;
@@ -7,7 +24,7 @@ import java.util.Set;
 /**
  * WebRTC client (not thread safe)
  *
- * @author  Philippe Proulx <philippe.proulx@savoirfairelinux.com>
+ * @author Philippe Proulx <philippe.proulx@savoirfairelinux.com>
  */
 public class WebRtcClient {
 	private final HashMap<WebRtcClient, WebRtcConnection> connections = new HashMap<WebRtcClient, WebRtcConnection>();
--- portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/WebRtcManager.java
+++ portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/WebRtcManager.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2013 Savoir-faire Linux
+ *     Philippe Proulx <philippe.proulx@savoirfairelinux.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by Savoir-faire Linux.  The name of Savoir-faire Linux
+ * may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 package com.liferay.chat.video;
 
 import java.util.ArrayList;
@@ -13,7 +30,7 @@ import java.util.Set;
  * follow that connection and manage mails for clients. Mails are added
  * automatically to users' mailboxes with the {@code processMsg*} methods.
  *
- * @author  Philippe Proulx <philippe.proulx@savoirfairelinux.com>
+ * @author Philippe Proulx <philippe.proulx@savoirfairelinux.com>
  */
 public class WebRtcManager {
     /*
