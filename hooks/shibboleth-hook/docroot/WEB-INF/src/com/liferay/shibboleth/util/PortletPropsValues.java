2ea79a4b1004ffbec7b1bb0e8b252eb7587bb030
Eric Chin <eric.chin@liferay.com>
2013-12-12 11:47:00 -0800

    LPS-42880 Add hook with logout action for Shibboleth

---
+++ hooks/shibboleth-hook/docroot/WEB-INF/src/com/liferay/shibboleth/util/PortletPropsValues.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.shibboleth.util;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.util.portlet.PortletProps;
+
+/**
+ * @author Eric Chin
+ */
+public class PortletPropsValues {
+
+	public static final boolean SHIBBOLETH_LOGOUT_ENABLED =
+		GetterUtil.getBoolean(
+			PortletProps.get(PortletPropsKeys.SHIBBOLETH_LOGOUT_ENABLED));
+
+	public static final String SHIBBOLETH_LOGOUT_URL = GetterUtil.getString(
+		PortletProps.get(PortletPropsKeys.SHIBBOLETH_LOGOUT_URL));
+
+}
