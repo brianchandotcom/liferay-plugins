470211d02da2e8975b88ac1f20d0148ceeac7886
Bruno Farache <bruno.farache@liferay.com>
2014-10-22 16:55:35 -0300

    LPS-51052 Regen

--- portlets/push-notifications-portlet/docroot/WEB-INF/service/com/liferay/pushnotifications/service/PushNotificationsEntryLocalServiceUtil.java
+++ portlets/push-notifications-portlet/docroot/WEB-INF/service/com/liferay/pushnotifications/service/PushNotificationsEntryLocalServiceUtil.java
@@ -53,6 +53,13 @@ public class PushNotificationsEntryLocalServiceUtil {
 		return getService().addPushNotificationsEntry(pushNotificationsEntry);
 	}
 
+	public static com.liferay.pushnotifications.model.PushNotificationsEntry addPushNotificationsEntry(
+		long userId, long parentEntryId,
+		com.liferay.portal.kernel.json.JSONObject payload) {
+		return getService()
+				   .addPushNotificationsEntry(userId, parentEntryId, payload);
+	}
+
 	/**
 	* Creates a new push notifications entry with the primary key. Does not add the push notifications entry to the database.
 	*
@@ -243,6 +250,18 @@ public class PushNotificationsEntryLocalServiceUtil {
 		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
 		throws java.lang.Throwable {
 		return getService().invokeMethod(name, parameterTypes, arguments);
+	}
+
+	public static void sendPushNotification(
+		com.liferay.portal.kernel.json.JSONObject jsonObject, int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		getService().sendPushNotification(jsonObject, start, end);
+	}
+
+	public static void sendPushNotification(long toUserId,
+		com.liferay.portal.kernel.json.JSONObject jsonObject, int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		getService().sendPushNotification(toUserId, jsonObject, start, end);
 	}
 
 	/**
