2abcc33fd64b0a2821fe936d9f94579cc30df809
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-08-17 10:07:35 -0300

    LPS-57810 Regenerate

--- portlets/calendar-portlet/docroot/WEB-INF/service/com/liferay/calendar/service/persistence/CalendarBookingExportActionableDynamicQuery.java
+++ portlets/calendar-portlet/docroot/WEB-INF/service/com/liferay/calendar/service/persistence/CalendarBookingExportActionableDynamicQuery.java
@@ -31,7 +31,6 @@ import com.liferay.portal.kernel.lar.StagedModelDataHandler;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerRegistryUtil;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.kernel.lar.StagedModelType;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.util.PortalUtil;
 
 /**
@@ -85,18 +84,12 @@ public class CalendarBookingExportActionableDynamicQuery
 			dynamicQuery.add(disjunction);
 		}
 
+		StagedModelDataHandler<?> stagedModelDataHandler = StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(CalendarBooking.class.getName());
+
 		Property workflowStatusProperty = PropertyFactoryUtil.forName("status");
 
-		if (_portletDataContext.isInitialPublication()) {
-			dynamicQuery.add(workflowStatusProperty.ne(
-					WorkflowConstants.STATUS_IN_TRASH));
-		}
-		else {
-			StagedModelDataHandler<?> stagedModelDataHandler = StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(CalendarBooking.class.getName());
-
-			dynamicQuery.add(workflowStatusProperty.in(
-					stagedModelDataHandler.getExportableStatuses()));
-		}
+		dynamicQuery.add(workflowStatusProperty.in(
+				stagedModelDataHandler.getExportableStatuses()));
 	}
 
 	protected StagedModelType getStagedModelType() {
